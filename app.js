c3e651919f4679518f08b061242668264c664a25
Melvin Carvalho <melvincarvalho@gmail.com>
2023-04-24 14:38:44 +0200

    add app button

--- app.js
+++ app.js
@@ -131,3 +131,65 @@ updateButton.addEventListener('click', () => {
 });
 
 document.body.appendChild(updateButton);
+
+
+
+// ... (previous code remains the same)
+function addAppSquare(appInfo) {
+  const index = mainEntity.app.length;
+  if (index % 3 === 0) {
+    appRow = document.createElement('tr');
+    tableBody.appendChild(appRow);
+  }
+
+  const appCell = document.createElement('td');
+  const backgroundColor = index % 6 < 3 ? '#e0f2ff' : '#c8e1ff';
+  appCell.setAttribute('style', `border: 1px solid #ccc; padding: 10px; width: 33%; height: 0; padding-bottom: 20%; position: relative; background-color: ${backgroundColor};`);
+
+  const appLink = document.createElement('a');
+  appLink.href = appInfo['@id'];
+  appLink.target = '_blank';
+  appLink.textContent = appInfo.label;
+  appLink.setAttribute('style', 'text-decoration: none; color: #1a73e8; font-weight: bold; font-size: 1.2em; position: absolute; top: 0; left: 0; right: 0; bottom: 0; display: flex; align-items: center; justify-content: center;');
+
+  const editIcon = document.createElement('span');
+  editIcon.innerHTML = '&#9998;'; // Pencil icon (Unicode character)
+  editIcon.setAttribute('style', 'position: absolute; top: 5px; right: 5px; cursor: pointer;');
+  editIcon.addEventListener('click', (event) => {
+    event.preventDefault();
+    const newLabel = prompt('Enter a new label:', appInfo.label);
+    if (newLabel !== null && newLabel.trim() !== '') {
+      appInfo.label = newLabel.trim();
+      appLink.textContent = appInfo.label;
+    }
+  });
+
+
+  appCell.appendChild(appLink);
+  appCell.appendChild(editIcon);
+  appRow.appendChild(appCell);
+  mainEntity.app.push(appInfo['@id']);
+
+}
+
+// ... (rest of the code remains the same)
+
+const addButtonCell = document.createElement('td');
+addButtonCell.setAttribute('style', 'border: 1px solid #ccc; padding: 10px; width: 33%; height: 0; padding-bottom: 20%; position: relative; background-color: #fff;');
+const addButton = document.createElement('button');
+addButton.textContent = '+';
+addButton.setAttribute('style', 'font-size: 2em; position: absolute; top: 0; left: 0; right: 0; bottom: 0; display: flex; align-items: center; justify-content: center; width: 100%; height: 100%; border: none; background-color: transparent;');
+addButton.addEventListener('click', () => {
+  const uri = prompt('Enter the app URI:');
+  if (uri !== null && uri.trim() !== '') {
+    const label = prompt('Enter the app label:');
+    if (label !== null && label.trim() !== '') {
+      const appInfo = { '@id': uri.trim(), label: label.trim() };
+      addAppSquare(appInfo);
+    }
+  }
+});
+
+addButtonCell.appendChild(addButton);
+appRow.appendChild(addButtonCell);
+// ... (rest of the code remains the same)
